fda39670323908e3025fa4dba4f518e270836e74
Brian Leiva <bridleiva@gmail.com>
2024-11-22 01:28:04 -0600

    luego de limpiar los comentarios, código funcional

--- sketch_nov11a.ino
+++ sketch_nov11a.ino
@@ -19,12 +19,12 @@ void ComputePID()
 {
     /*Cuánto tiempo ha pasado desde el último cálculo*/
     unsigned long now = millis();
-    double timeChange = (double)(now - lastTime);
+    double timeChange = (double)((now - lastTime)/1000);
 
     /*Calcular todas las variables de error de trabajo*/
     double error = Setpoint - Input;
     errSum += (error * timeChange);
-    double dErr = (error - lastErr);  // / timeChange;
+    double dErr = (error - lastErr)/ timeChange;
 
     /*Calcular salida PID*/
     Output = kp * error + ki * errSum + kd * dErr;
@@ -42,14 +42,14 @@ void setup() {
     ki = 0.1;
     kd = 0.01;
 
-    Setpoint = 28.0;
     Input = 0;
-    //Output = 0;
+    Setpoint = 30.0;
     
     pinMode(PWM_PIN, OUTPUT);
 
     // Iniciar comunicación serial a una velocidad de 9600 baudios:
     Serial.begin(9600);
+    
     // Iniciar la biblioteca:
     sensors.begin();
 }
@@ -61,24 +61,14 @@ void loop() {
     // Obtener la temperatura en grados Celsius para el índice del dispositivo:
     float tempC = sensors.getTempCByIndex(0); // el índice 0 se refiere al primer dispositivo
 
-    //Input = constrain(tempC, 0 ,125);
     Input = tempC;
     ComputePID();
 
-   // Output = constrain(Output, 0, 125);
-
-    // Mapear la salida PID al rango PWM (0-255) e invertirlo
-    //int pwmOutput = map(Output, 0, 255, 255, 0);
-    //int pwmOutput = map(Output, -125, 125, 0, 255); //directo para que trabaje solo con el mofstet
-    float coeficiente = Output / 10000;
+    float coeficiente = Output / 50;
     int pwmOutput = int(255 * coeficiente);
 
     // Limitar la salida PWM al rango válido
-    //pwmOutput = constrain(pwmOutput, 100, 255);
     pwmOutput = constrain(pwmOutput, 0, 100);  // para trabajar solo con el mofset
-    
-
-    //pwmOutput = 0;
 
     // Escribir la salida PWM al pin
     analogWrite(PWM_PIN, pwmOutput);
